beaf286adef50de52ac71accadffd543891592a4
salaza21 <sal16024@byui.edu>
2019-10-14 10:54:54 -0600

    part 4

---
+++ weather/locations/js/library.js
@@ -0,0 +1,27 @@
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+function footerDate() {
+    var date = new Date();
+    var realDate = date.getDate();
+    var day = new Date();
+    var numDay = day.getDay();
+    var monthy = new Date();
+    var mm = monthy.getMonth();
+    var yyyy = new Date();
+    var year = yyyy.getFullYear();
+
+    var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var nameWeek = week[numDay];
+
+    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var nameMonth = month[mm];
+
+    var fullDate = nameWeek + "," + " " + realDate + " " + nameMonth + " " + year;
+
+    document.getElementById("today").innerHTML = fullDate;
+
+}
+
+footerDate();
